0a96b8dbdbdda43a3f0bea0ca2b7af0f2d04ecf9
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2016-02-24 12:48:43 +0900

    レポジトリを作成するAPIを叩くことに成功 fix #19

--- content_scripts/content_scripts.js
+++ content_scripts/content_scripts.js
@@ -155,14 +155,8 @@ function filesendAPIararysis(username, repo, pass) {
     xhr.send(null);
 }
 
-
-
-
-
-
-
 function createrepo(username,pass,repo){
-    //defaultcreaterepo
+    //新しくレポジトリを作る。stringifyを使うと成功した。
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
 
@@ -180,60 +174,6 @@ function createrepo(username,pass,repo){
     let basic = window.btoa(unescape(encodeURIComponent(username + ':' + pass)));
     xhr.setRequestHeader('authorization', 'Basic ' + basic);
 
-    //xhr.setRequestHeader('name', repo);
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.setRequestHeader('cache-control', 'no-cache');
-
-    xhr.send(data);
-}
-function createrepoformobj(username,pass,repo){
-    //成功しない為、new formを使う。
-    var formdata=new FormData();
-    formdata.append("description","xhr");
-    formdata.append("name",repo);
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    
-    let data={
-        'description':'xhr',
-        'name':repo
-    };
-
-    xhr.addEventListener('readystatechange', function () {
-        if (this.readyState === 4) {
-            console.log(this.responseText);
-        }
-    });
-    xhr.open('POST', 'https://api.github.com/user/repos');
-    let basic = window.btoa(unescape(encodeURIComponent(username + ':' + pass)));
-    xhr.setRequestHeader('authorization', 'Basic ' + basic);
-
-    //xhr.setRequestHeader('name', repo);
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.setRequestHeader('cache-control', 'no-cache');
-
-    xhr.send(formdata);
-}
-function createrepoatfetch(username,pass,repo){
-    //fetch
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    
-    let data={
-        'description':'xhr',
-        'name':repo
-    };
-
-    xhr.addEventListener('readystatechange', function () {
-        if (this.readyState === 4) {
-            console.log(this.responseText);
-        }
-    });
-    xhr.open('POST', 'https://api.github.com/user/repos');
-    let basic = window.btoa(unescape(encodeURIComponent(username + ':' + pass)));
-    xhr.setRequestHeader('authorization', 'Basic ' + basic);
-
-    //xhr.setRequestHeader('name', repo);
     xhr.setRequestHeader('content-type', 'application/json');
     xhr.setRequestHeader('cache-control', 'no-cache');
 
